5a1f6b2168ec77aee31cb07eb77b204d64ba326a
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-30 17:15:15 +0300

    Task1_CountPositinNumM

---
+++ Task1_CountPositinNumM/Program.cs
@@ -0,0 +1,16 @@
+﻿// вывести сколько чисел > 0 из M введённых с клавиатуры 
+// пользователем чисел (число M задаёт сам пользователь)
+
+Console.WriteLine("Введите число M - скольо раз будут задаваться числа пользователем");
+int count = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int CountPositiveNum = 0;
+
+for (int i = 0; i < count; i++)
+{
+  Console.WriteLine("Введите произольное положительное или отрицательное число");
+  int num = Convert.ToInt32(Console.ReadLine());
+  if(num > 0) CountPositiveNum = CountPositiveNum + 1;
+}
+Console.WriteLine();
+Console.WriteLine($"Количество положительных чисел равно {CountPositiveNum}");
